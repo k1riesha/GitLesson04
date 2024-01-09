91c853c28ea5d088d19ff48a04a7d00eb395813d
k1riesha <kiriesha999@gmail.com>
2024-01-09 20:43:48 +0200

    add new system out println

--- src/Lesson4GitProgram.java
+++ src/Lesson4GitProgram.java
@@ -2,5 +2,6 @@ public class Lesson4GitProgram {
 
     public static void main(String[] args){
         System.out.println("Hello world from Kira to Git");
+        System.out.println("Second commit to git");
     }
 }
